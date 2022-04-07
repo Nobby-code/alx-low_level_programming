b14212df28401c9ce0c82e0b972efaea62cd886d
Nobby-code <norbertguda0@gmail.com>
2022-04-07 06:44:36 -0700

    printf in c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main() - is the entry point of the program
- *
  * printf - inbuilt func to print out 
  * return - means no return value
  */
-int main()
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
