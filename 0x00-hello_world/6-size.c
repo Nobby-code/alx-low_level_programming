83c56c58eb3599c6a7d74844a280b1a5c7d160b3
Nobby-code <norbertguda0@gmail.com>
2022-04-08 00:23:42 -0700

    Printing the size of c datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %d", sizeof(char));
-printf("Size of an int: %d", sizeof(int));
-printf("size of a long int: %d", sizeof(long));
-printf("size of a long long int: %d", sizeof(long long));
-printf("Size of a float: %d", sizeof(double));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d bytes(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long));
+printf("Size of a float: %d byte(s)\n", sizeof(double));
 return (0);
 }
