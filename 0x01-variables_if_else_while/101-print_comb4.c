b34e04b0d954cd548d0cc27224805d0be7836ea7
Nobby-code <norbertguda0@gmail.com>
2022-04-09 21:26:37 -0700

    Printing all possible combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - Prints any possible combination of three digits separated by commas, using putchar
+ *
+ *Return: Always returns 0
+ */
+int main(void)
+{
+	int i,j,k;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if (i != j && j != k && k != i)
+				{
+					putchar(i = '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					putchar (',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
