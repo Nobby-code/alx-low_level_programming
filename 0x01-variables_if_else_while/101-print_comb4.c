405f674e4ec05c826961da81e9f360b7d480529b
Nobby-code <norbertguda0@gmail.com>
2022-04-09 21:53:47 -0700

    Printing all possible combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 			{
 				if (i != j && j != k && k != i)
 				{
-					putchar(i = '0');
+					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
 					putchar (',');
