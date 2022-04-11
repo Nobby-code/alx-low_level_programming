4d8e5fc94b5681fbc564af75319e3b8e76f6e878
Nobby-code <norbertguda0@gmail.com>
2022-04-11 01:51:22 -0700

    Printing alphabet in lowercase nexcept q and e using putchar

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-if (ch != 'q' && ch != 'e')
-{
-putchar(ch);
-}
-}
-putchar('\n');
-return (0);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
