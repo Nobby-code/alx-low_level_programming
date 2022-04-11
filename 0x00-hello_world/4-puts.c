e9060667726058c88385d621d9d70d3236fa633d
Nobby-code <norbertguda0@gmail.com>
2022-04-10 23:36:06 -0700

    Printing characters using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *main - entry point of the program
+ *main -  entry point of the program
  *
- *Return: 0 after printing the function
+ *Return: Always return 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
 }
