afdf1b8affed88d26cc6b6a3d877df3960fadded
Nobby-code <norbertguda0@gmail.com>
2022-04-11 02:38:14 -0700

    Printing 1-9 separated by commas using putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,18 @@
  */
 int main(void)
 {
-int i;
-for (i = 0; i <= 9 ; i++)
-{
-	putchar(i + '0');
-	if (i == 9)
+	int i;
+
+	for (i = 0; i <= 9 ; i++)
 	{
-		break;
+		putchar(i + '0');
+		if (i == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar (' ');
 	}
-	putchar(',');
-	putchar (' ');
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
