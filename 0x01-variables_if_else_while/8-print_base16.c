071ac3608fc355e7f17c036a1833a1ead34c89e9
Nobby-code <norbertguda0@gmail.com>
2022-04-11 02:31:07 -0700

    Printing base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-int num;
-char letter;
-for (num = 0; num <= 9; num++)
-{
-putchar(num + '0');
-}
-for (letter = 'a'; letter <= 'f'; letter++)
-{
-putchar(letter);
-}
-putchar('\n');
-return (0);
+	int num;
+	char letter;
+	
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
 }
