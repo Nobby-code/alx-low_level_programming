822bbf61b0dca617693ee0aa85c20de60375f572
Nobby-code <norbertguda0@gmail.com>
2022-04-11 01:09:06 -0700

    Printing using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - Entry point of the program
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
  
-	return(1);
+	return (1);
 }
