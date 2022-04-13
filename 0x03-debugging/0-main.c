6eb7380a14462f5fb370b5a82c70744ca1311b2f
Nobby-code <norbertguda0@gmail.com>
2022-04-13 04:42:33 -0700

    Testing if 0 is positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  *main - Entry point of the program to return another function
- *
- * Return: 0 success
+ *Return: 0 success
  */
+
 int main(void)
 {
 	int value;
 
 	value = 0;
 	positive_or_negative(value);
+
 	return (0);
 }
