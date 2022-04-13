cb63e8105aa464dd00bdc39cfcea11bae0d05e50
Nobby-code <norbertguda0@gmail.com>
2022-04-13 03:43:18 -0700

    Testing a function in main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point of the program to return another function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
