55eb33eac269bc34d51398893d7aed83c9d40dc1
Nobby-code <norbertguda0@gmail.com>
2022-04-14 01:54:18 -0700

    Printing 0-9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints 0-9 except 2 and 4
+ *@void: no function argument
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	int = 0;
+	while (i < 10)
+	{
+		if (i == 2 && i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
