1c5ff78eff582f93f4bda7c62ed711acba6151de
Nobby-code <norbertguda0@gmail.com>
2022-04-14 04:20:11 -0700

    Function that drwas a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_diagonal - printing diagonal line a given times
+ *@n: funtion argument
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int b;
+
+	if (i > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (b = 0; b < i; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
