bd47ec6efa6c9e4e4d77dfb85172f75d1b3a1a4f
Nobby-code <norbertguda0@gmail.com>
2022-04-14 11:24:57 -0700

    Printing 0-9 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *more_numbers - function to print 00-9 ten times
+ *more_numbers - function to print 0-9 ten times
  *@void: no argument
  *
  */
@@ -10,13 +10,15 @@ void more_numbers(void)
 	int i, j;
 
 	for (i = 0; i <= 9; i++)
+	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j / 10 != 0)
+			if (j >= 10)
 			{
-				_putchar(j / 10 + '0');
+				_putchar((j / 10) + '0');
 			}
-			_putchar(j % 10 + '0');
+			_putchar((j % 10) + '0');
 		}
 	_putchar('\n');
+	}
 }
