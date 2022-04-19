b6ed441f94dd58eddc8f395eb0fb53887a31daaf
Nobby-code <norbertguda0@gmail.com>
2022-04-19 04:45:38 -0700

    Converting a date to the day of a year and determine how many days are left in a year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- *print_remaining_days - prints days in a yeas
- *@month: takes in months
- *@day: counts days
- *@year: takes in year
+ *print_remaining_days - takes a date and prints how many days are
+ *left in the year, taking leep year into account
+ *@month: takes month in number format
+ *@day: day of month
+ *@year: year
  *Return: void
- *
  */
+
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
