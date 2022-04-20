20aa163572fd8aebb35f633b67b29d18b9bdfbd6
Nobby-code <norbertguda0@gmail.com>
2022-04-20 05:45:44 -0700

    5-rev_string to reverse the order of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a tring order
+ *@s: argument
+ *Return: void success
+ *
+ */
+
+void rev_string(char *s)
+{
+	char *start_c, *end_c, c;
+	int i, count, length;
+
+	length = 0;
+	for (i = 0; s[i], i++)
+	{
+		length++;
+	}
+
+	count = length;
+
+	start_c = s;
+	end_c =  s;
+
+	for (i = 0; i < count - 1; i++)
+	{
+		end_c++;
+	}
+
+	for (i = 0; i < count / 2; i++)
+	{
+		c = *end_c;
+		*end_c =  *start_c;
+		*star_c =  c;
+
+		start_c++;
+		end_c--;
+	}
+}
