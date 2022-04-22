7b8176e98710c521ff26b23feef93e85a013edad
Nobby-code <norbertguda0@gmail.com>
2022-04-22 13:44:54 -0700

    0-strcat to concatenate two character strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * *_strcat - function to concatenate string
+ * @dest: agrgument
+ *@src: argument
+ *Return: return dest if success
  */
+
+char *_strcat(char *dest, char *src)
+{
+	char len1, len2;
+
+	len1 = 0;
+	while (dest[len1] != 0)
+	{
+		len1++;
+	}
+	for (len2 = 0; src[len2] != '\0', len2++)
+	{
+		dest[len1] = src[len2];
+
+	}
+
+	return (dest);
+}
