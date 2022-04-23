83c9415b3ed1c181c1d421977bd2e37e1ff4089f
Nobby-code <norbertguda0@gmail.com>
2022-04-23 01:32:43 -0700

    0-strcat to concatinate  string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,17 @@ char *_strcat(char *dest, char *src)
 	char len1, len2;
 
 	len1 = 0;
-	while (dest[len1] != '\0')
+	while (dest[len1])
 	{
 		len1++;
 	}
-	for (len2 = 0; src[len2] != '\0', len2++)
+	for (len2 = 0; src[len2] != 0, len2++)
 	{
 		dest[len1] = src[len2];
+		len1++;
 
 	}
+	dest[len1] = '\0';
 
 	return (dest);
 }
