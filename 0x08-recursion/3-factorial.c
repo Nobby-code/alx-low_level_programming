ab54359f90f3471f8ae35ba5757f02fa368d6179
Nobby-code <norbertguda0@gmail.com>
2022-04-29 01:11:58 -0700

    Gettinf factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *factorial - gets the factorial of a number n
+ *@n: the integers to get the factorial of
+ *
+ *Return: 1 if n=0 otherwise factorial of a number
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
