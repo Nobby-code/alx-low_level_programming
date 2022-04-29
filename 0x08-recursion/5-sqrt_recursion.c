b1888c706e2ef598791100cf3621583fe4a5b677
Nobby-code <norbertguda0@gmail.com>
2022-04-29 04:12:02 -0700

    Squaeroot of a number using recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ *getSqrt - function to make getting squareroot possible
+ *@a: first argument
+ *@b: second argument
+ *
+ * Return: return an integer value of the square
+ */
+
+int getSqrt(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (getSqrt(a, b - 1));
+}
+
+
+/**
  *_sqrt_recursion - function to return sqrt
  *@n: digit to get  its squareroot
  *
@@ -9,15 +31,5 @@
 
 int _sqrt_recursion(int n)
 {
-	int a;
-
-	if (a * a == n)
-	{
-		return (a);
-	}
-	else if (a * a > n || a * a < n)
-	{
-		return (-1);
-	}
-	return (_sqrt_recursion(n - 1));
+	return (getSqrt(n, 1));
 }
