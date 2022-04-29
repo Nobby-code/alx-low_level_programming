2180f04ebb0723ed0789fcf6202c726e92835348
Nobby-code <norbertguda0@gmail.com>
2022-04-28 23:18:21 -0700

    0-puts_recursion t print out characters

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - function to print out characters
+ *@s: char to be printed
+ *
+ *Return: return void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s++);
+}
