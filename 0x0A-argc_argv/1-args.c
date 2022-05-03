b218d946a3ab2b8092c55a5bb6104718bba1bdf1
Nobby-code <norbertguda0@gmail.com>
2022-05-03 07:18:44 -0700

    1-args to print the arguments of a program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - the program entry point
+ *@argc: the number of arguments
+ *@argv@: vector
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		argv[i];
+	}
+	printf("%d\n", argc);
+
+	return (0);
+}
