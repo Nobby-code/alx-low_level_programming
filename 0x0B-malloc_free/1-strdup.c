d4b2d9cdf254ba60728b54df6517811fda76bb8a
Nobby-code <norbertguda0@gmail.com>
2022-05-06 03:00:56 -0700

    Function that returns a pointer to a newly allocated memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strlen - function to return length
+ *@s: char type
+ *Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		return (a);
+}
+/**
+ *_strdup - function that duplicates a string
+ *@str: the string to be duplicated
+ *
+ *Return: to return the duplicated string if success
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	int size;
+
+	char *spc;
+
+	if (str == NULL)
+		return (NULL);
+	size = _strlen(str) + 1;
+
+	spc = malloc(size * sizeof(char));
+
+	i = 0;
+	while (i < size)
+		if (spc == NULL)
+			return (NULL);
+		spc[i] = str[i];
+		i++;
+	printf("%s", spc);	
+	return (spc);
+}
