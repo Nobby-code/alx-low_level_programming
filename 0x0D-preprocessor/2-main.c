65b9d5c85e7ae565fc0b944803827f3d95bc7c0a
Nobby-code <norbertguda0@gmail.com>
2022-05-12 08:20:18 -0700

    Program to print the name of the file using macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - The program to print the file name
+ *@void: no argument
+ *
+ *Return: returns the file name
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+}
