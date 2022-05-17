14005dbfe7349499bbc42fc823e34bfc5b056893
Nobby-code <norbertguda0@gmail.com>
2022-05-17 03:58:52 -0700

    Function that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *op_add - function to add
+ *@a: first argument
+ *@b: second argument
+ *
+ *Return: Always 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - function to subtract
+ *@a: first argument
+ *@b: second argument
+ *
+ * Return: Always 0
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - function to multiply
+ *@a: the first argument
+ *@b: the second argument
+ *
+ * Return: Always 0
+ */
+
+int op_mul(int a, int)
+{
+	return (a - b);
+}
+
+/**
+ *op_div - function to divide
+ *@a: the first argument
+ *@b:second argument
+ *
+ * Return: Always 0
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - fonction for module
+ *@a: first argument
+ *@b: second argument
+ *
+ * Return: Always 0
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
