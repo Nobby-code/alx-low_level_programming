f69c60115c9cabbd0506f383ec8bc8bf7c010ac6
Nobby-code <norbertguda0@gmail.com>
2022-05-24 22:41:44 -0700

    Additing of README.md file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,16 +13,15 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str)
-		{
-			printf("[%u] %s\n", h->len, h->str);
-		}
-		else
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		i++;
-
 		h = h->next;
 	}
 
