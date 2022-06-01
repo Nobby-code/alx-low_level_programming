844a5f4a65be20d070a34120a8ee2f77ff73bb32
Nobby-code <norbertguda0@gmail.com>
2022-06-01 04:56:17 -0700

    Function to set value of a bit at a gine index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *set_bit - function to set the value of a it to a given number
+ *@n: the number to be set
+ *@index: the index too inset the number
+ *
+ *Return: 1 on success, -1 otherwise
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int val;
+
+	if (index >= 64)
+		return (-1);
+	else
+	{
+		val = 1 << index;
+		*n = *n | val;
+		return (1);
+	}
+}
