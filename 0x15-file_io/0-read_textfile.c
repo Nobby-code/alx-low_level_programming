18abf7ea57493d37937876dd6906735fd03045be
Nobby-code <norbertguda0@gmail.com>
2022-06-02 07:05:34 -0700

    Function to read a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,42 @@
 
 #include "main.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stddef.h>
 
 /**
+ *read_textfile - function to read file
+ *@filename: the name of the file to be written
+ *@letters: the letterdrs in  a file
  *
- *
- *
- *
- *
+ *Return: returns the letter
  *
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int
+	char *mem;
+	ssize_t wf, of, rf, af;
+
+	of = open(filename, O_RDONLY);
+	rf = read(of, mem, letters);
+	wf = write(STDOUT_FILENO, mem, rf);
+
+	if (filename == NULL || mem == NULL)
+		return (0);
+	if ((of || rf || wf) == -1)
+	{
+		free(mem);
+		return (0);
+	}
+	close(of);
+	return (wf);
+}
+
+int main(int ac, char **av)
+{
+	return (0);
 }
