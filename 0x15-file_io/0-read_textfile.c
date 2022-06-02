419f70c0e70b698c1be91591e6a04fe8ed02a5ff
Nobby-code <norbertguda0@gmail.com>
2022-06-02 12:44:49 -0700

    Function to read text file and print it out

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,6 @@
 
 #include "main.h"
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stddef.h>
 
 /**
  *read_textfile - function to read file
@@ -21,17 +16,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *mem;
 	ssize_t wf, of, rf, af;
 
+	if (filename == NULL)
+		return (0);
+	mem = malloc(sizeof(char) * letters);
+
+	if (mem == NULL)
+		return (0);
+
 	of = open(filename, O_RDONLY);
 	rf = read(of, mem, letters);
 	wf = write(STDOUT_FILENO, mem, rf);
 
-	if (filename == NULL || mem == NULL)
-		return (0);
 	if ((of || rf || wf) == -1)
 	{
 		free(mem);
 		return (0);
 	}
+	free(mem);
 	close(of);
+
 	return (wf);
 }
