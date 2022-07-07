00be7ef5e2ca5811b2c5c3abe6561e76b4a2e4ee
Nobby-code <norbertguda0@gmail.com>
2022-07-07 14:00:23 -0700

    lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -9,6 +14,7 @@
  * Description: doubly linked list node structure
  * 
  */
+
 typedef struct dlistint_s
 {
     int n;
@@ -18,5 +24,6 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif /*end if*/
