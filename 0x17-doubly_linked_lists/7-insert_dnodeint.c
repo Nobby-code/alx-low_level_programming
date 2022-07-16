8edb46e1bad8b443eb5a2dc399f3e37a6f05a42f
Nobby-code <norbertguda0@gmail.com>
2022-07-16 15:43:53 -0700

    7-insert_dnodeint.c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,80 @@
+#include "lists.h"
+
+/**
+ *insert_at_index - function to insert node at a specific index
+ *@h: pointer to the header node
+ *@idx: the index where the node should be inserted
+ *@n: the integer number to be inserted
+ *Return: return the new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp, *new_node;
+	unsigned int count = 0;
+	new_node = malloc(sizeof(dlistint_t));
+	
+	if ((new_node == NULL) || (!idx))
+	{
+        	return (NULL);
+	}
+	
+	temp = *h;
+	while(temp)
+	{
+		new_node->n = n;
+		if (count == idx - 1)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			new_node->prev = temp;
+			if (new_node->next != NULL)
+			{
+				new_node->next->prev = new_node;
+			}
+		}
+		temp = temp->next;
+		count++;
+	}
+	return (new_node);
+
+
+}
+
+/*
+dlistint_t *insert_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h;
+	dlistint_t new_node;
+	unsigned int index = idx - 1;
+
+	new_node = malloc(sizeof(dlistint_t));
+	new_node->n = n;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		
+		*h = new_node;
+		return (*h);
+	}
+
+	int i = 0;
+
+	while (temp && i != index)
+	{
+		i++;
+		temp = temp->next;
+	}
+
+	if (i == index && temp)
+	{
+
+	}
+}*/
