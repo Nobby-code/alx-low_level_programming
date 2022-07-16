319f3c838e605fbcfae380bef5e62ebcfecc1b14
Nobby-code <norbertguda0@gmail.com>
2022-07-15 18:52:30 -0700

    jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Nobby-code/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackport/libtest.so
+wget -P /tmp https://github.com/Nobby-code/alx-low_level_programming/\
+	raw/master/0x18-dynamic_libraries/jackport/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
--- 0x18-dynamic_libraries/jackport/test.c
+++ 0x18-dynamic_libraries/jackport/test.c
@@ -11,6 +11,6 @@
 int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 + 9\n", 17);
- 	write(1, "\nCongratulations, you won the Jackpot!", 37);
+	write(1, "\nCongratulations, you won the Jackpot!", 37);
 	exit(EXIT_SUCCESS);
 }
