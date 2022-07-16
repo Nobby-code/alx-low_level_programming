0eba816235d43ebf939abcd6318e8c8b89949a7b
Nobby-code <norbertguda0@gmail.com>
2022-07-15 19:20:42 -0700

    jackpot

--- 0x18-dynamic_libraries/jackport/test.c
+++ 0x18-dynamic_libraries/jackport/test.c
@@ -11,6 +11,6 @@
 int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 + 9\n", 17);
-	write(1, "\nCongratulations, you won the Jackpot!", 37);
+	write(1, "Congratulations, you won the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
