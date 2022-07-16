a86b44165d20553b5aaaca700a2bfe80645eba2a
Nobby-code <norbertguda0@gmail.com>
2022-07-15 18:54:39 -0700

    jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Nobby-code/alx-low_level_programming/\
-	raw/master/0x18-dynamic_libraries/jackport/libtest.so
+wget -P /tmp https://github.com/Nobby-code/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackport/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
